3cf19fca31d5b569661215f4b639eaf67e7894f8
Al-Bykov <a.bykov.work@gmail.com>
2020-04-10 19:54:29 +0300

    3.3. Разбор первой программы.[#271674]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexandr Bykov");
+        System.out.println("23.06.1986");
+
+    }
+}
