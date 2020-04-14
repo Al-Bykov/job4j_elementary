1b8cbb2d3f86c79fcbdf5b4208dd0f3c3e01f093
Al-Bykov <a.bykov.work@gmail.com>
2020-04-14 17:42:11 +0300

    2. Тесты для идеальный вес[#271691]

--- src/ru/job4j/calculator/FitTest.java
+++ src/ru/job4j/calculator/FitTest.java
@@ -8,7 +8,7 @@ public class FitTest {
     @Test
     public void whenManHeight186ThenFit98() {
         int in = 186;
-        double expected = 98.89999999999999;
+        double expected = 98.89;
         double out = Fit.manWeight(in);
         Assert.assertEquals(expected, out, 0.01);
     }
@@ -16,7 +16,7 @@ public class FitTest {
     @Test
     public void whenWomanHeight176ThenFit75() {
         int in = 176;
-        double expected = 75.89999999999999;
+        double expected = 75.89;
         double out = Fit.womanWeight(in);
         Assert.assertEquals(expected, out, 0.01);
     }
