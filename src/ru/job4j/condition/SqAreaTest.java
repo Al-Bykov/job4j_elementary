ea9cb6c129b26c88be7784f3b472629212066690
Al-Bykov <a.bykov.work@gmail.com>
2020-04-17 19:14:07 +0300

    4.5. Отладка программы в IDEA[#271618]

--- src/ru/job4j/condition/SqAreaTest.java
+++ src/ru/job4j/condition/SqAreaTest.java
@@ -1,25 +1,27 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
 public class SqAreaTest {
 
     @Test
-    public void whenP6K2ThenS2() {
-        int in1 = 6;
-        int in2 = 2;
-        double expected = 2;
-        double out = SqArea.square(in1, in2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
 
     @Test
-    public void whenP4K1ThenS1() {
-        int in1 = 4;
-        int in2 = 1;
-        double expected = 1;
-        double out = SqArea.square(in1, in2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void whenp6k2s2() {
+        int k = 2;
+        int p = 6;
+        int expected = 2;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
 }
