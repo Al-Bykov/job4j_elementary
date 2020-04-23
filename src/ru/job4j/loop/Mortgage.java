9ce716f3040608843969933379ba7c37c7ccbf93
Al-Bykov <a.bykov.work@gmail.com>
2020-04-23 19:42:13 +0300

    5.7. Ипотека.[#271706]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 1;
+        double debt = amount;
+        while ((debt + percent) >= salary) {
+            year++;
+            debt = (amount + percent) - salary;
+        }
+        return year;
+    }
+}
