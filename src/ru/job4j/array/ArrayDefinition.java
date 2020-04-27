cc034ee73139f551682934c46ee26517bc0baff6
Al-Bykov <a.bykov.work@gmail.com>
2020-04-27 13:54:55 +0300

    6.0.2. Заполнение массива.[#271643]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,20 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
+
+        /*short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        */
+
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Ivan Petrov";
+        names[2] = "Ivan Sidorov";
+        names[3] = "Petr Ivanov";
+
+        for (String message : names) {
+            System.out.println(message);
+        }
     }
 }
