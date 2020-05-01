271d9013eeddb3a679e3662024ff677040a85a3b
Al-Bykov <a.bykov.work@gmail.com>
2020-05-01 19:30:03 +0300

    6.2.2. Переставить элементы массива[#271648]

--- src/ru/job4j/array/SwitchArrayTest.java
+++ src/ru/job4j/array/SwitchArrayTest.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class SwitchArrayTwoTest {
+public class SwitchArrayTest {
 
     @Test
     public void whenSwap0to3() {
         int[] input = {1, 2, 3, 4};
         int[] expect = {4, 2, 3, 1};
-        int[] rsl = SwitchArrayTwo.swap(input, 0, input.length - 1);
+        int[] rsl = SwitchArray.swap(input, 0, input.length - 1);
         assertThat(rsl, is(expect));
     }
 
@@ -18,7 +18,7 @@ public class SwitchArrayTwoTest {
     public void whenSwap1to2() {
         int[] input = {1, 2, 3, 4};
         int[] expect = {1, 3, 2, 4};
-        int[] rsl = SwitchArrayTwo.swap(input, 1, 2);
+        int[] rsl = SwitchArray.swap(input, 1, 2);
         assertThat(rsl, is(expect));
     }
 
@@ -26,7 +26,7 @@ public class SwitchArrayTwoTest {
     public void whenSwap2to3() {
         int[] input = {1, 2, 3, 4};
         int[] expect = {1, 2, 4, 3};
-        int[] rsl = SwitchArrayTwo.swap(input, 2, 3);
+        int[] rsl = SwitchArray.swap(input, 2, 3);
         assertThat(rsl, is(expect));
     }
 }
